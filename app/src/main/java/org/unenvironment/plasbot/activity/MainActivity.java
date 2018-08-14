a3377c99e40f30ad91c2852fcf1f9e0a4f3dc8e9
= <=>
2018-08-14 06:15:16 +0300

    All PlasBot changes

--- app/src/main/java/org/unenvironment/plasbot/activity/MainActivity.java
+++ app/src/main/java/org/unenvironment/plasbot/activity/MainActivity.java
@@ -1,15 +1,177 @@
 package org.unenvironment.plasbot.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import org.unenvironment.plasbot.R;
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout btnOpenCamera, btnOpenGallery;
+
+    int OPEN_CAMERA_CODE = 100;
+    int OPEN_GALLERY_CODE = 101;
+
+    private Bitmap bitmap;
+
+    private RelativeLayout rvMessage;
+    private ImageView imgChosenImage;
+
+    String imageFilePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOpenCamera = findViewById(R.id.open_camera);
+        btnOpenGallery = findViewById(R.id.open_gallery);
+
+        imgChosenImage = findViewById(R.id.chosenImage);
+        rvMessage = findViewById(R.id.message);
+
+        btnOpenGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+        btnOpenCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+    }
+
+    private void openCamera(){
+//        Intent intent = new Intent();
+//
+//        intent.setType("image/*");
+//        intent.setAction(Intent.ACTION_GET_CONTENT);
+//        intent.addCategory(Intent.CATEGORY_OPENABLE);
+//
+//        startActivityForResult(intent, OPEN_CAMERA_CODE);
+        Intent pictureIntent = new Intent(
+                MediaStore.ACTION_IMAGE_CAPTURE
+        );
+
+        if(pictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            }catch (IOException ex){
+                Toast.makeText(this, "There was an issue creating the image file", Toast.LENGTH_SHORT).show();
+            }
+
+            if (photoFile != null){
+
+            }
+            startActivityForResult(pictureIntent,
+                    OPEN_CAMERA_CODE);
+        }
+    }
+
+    private void openGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,
+                "Select Picture"), OPEN_GALLERY_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            InputStream stream = null;
+            rvMessage.setVisibility(View.GONE);
+            imgChosenImage.setVisibility(View.VISIBLE);
+            if (requestCode == OPEN_CAMERA_CODE) {
+                if (data != null && data.getExtras() != null) {
+                    bitmap = (Bitmap) data.getExtras().get("data");
+
+                }
+            } else if (requestCode == OPEN_GALLERY_CODE) {
+                if (data != null && data.getExtras() != null) {
+                    Uri selectedImageUri = data.getData();
+                    imageFilePath = getPath(selectedImageUri);
+                    try {
+                        bitmap = getBitmapFromUri(selectedImageUri);
+                        imgChosenImage.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException{
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "IMG_" + timestamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        imageFilePath = image.getAbsolutePath();
+        return image;
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+    public String getPath(Uri uri) {
+        if( uri == null ) {
+            // TODO perform some logging or show user feedback
+            return null;
+        }
+        // try to retrieve the image from the media store first
+        // this will only work for images selected from gallery
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String path = cursor.getString(column_index);
+            cursor.close();
+            return path;
+        }
+        // this is our fallback here
+        return uri.getPath();
     }
 }
